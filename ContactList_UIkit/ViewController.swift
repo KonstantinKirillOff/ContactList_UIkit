7be614a31d4bb576b64c3e29e3029a7d2cb6f59e
kirill_off <kirilloffkostya@gmail.com>
2022-08-02 12:24:59 +0300

    Initial Commit

---
+++ ContactList_UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList_UIkit
+//
+//  Created by Konstantin Kirillov on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
